2710b01e0630425b2a95613009e982f726708016
Latiful Islam <litu7469@gmail.com>
2025-10-16 02:57:13 +0600

    Bind Flask to dynamic port for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
@@ -13,4 +14,5 @@ def predict():
     return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
